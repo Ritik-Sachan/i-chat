26a4512e851c685327b863f2abfe9a6ad7abebbf
Ritik-Sachan <93980908+Ritik-Sachan@users.noreply.github.com>
2023-05-28 17:38:46 +0530

    adding files

--- next.conf.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-module.exports={
-    env:{
-        REACT_APP_ACCESS_KEY : process.env.REACT_APP_ACCESS_KEY,
-        REACT_APP_API_URL:process.env.REACT_APP_API_URL
-    },
-    
-};
